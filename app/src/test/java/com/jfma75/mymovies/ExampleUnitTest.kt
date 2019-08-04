a1d783a27415e58a7668ff1e95d3edd5ee76d30d
Juan Francisco <jfmiranda75@hotmail.com>
2019-08-04 20:02:42 +0200

    Update ExampleUnitTest.kt

--- app/src/test/java/com/jfma75/mymovies/ExampleUnitTest.kt
+++ app/src/test/java/com/jfma75/mymovies/ExampleUnitTest.kt
@@ -85,7 +85,7 @@ class ExampleUnitTest {
     @Test
     fun `data is loaded from server`() {
         runBlocking {
-            val result = service.listMoviesAsync("batman", "b4a4fc89").await()
+            val result = service.listMoviesAsync("batman", "").await()
 
             assert(result.search?.size == 10)
         }
